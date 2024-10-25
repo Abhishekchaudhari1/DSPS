8fc9430b068f13a7306b0c7dbd568c62a6cd1856
Abhishek Chaudhari <143903535+Abhishekchaudhari1@users.noreply.github.com>
2024-10-25 14:36:00 +0530

    Update 9_Singly_LL.cpp

--- 9_Singly_LL.cpp
+++ 9_Singly_LL.cpp
@@ -13,64 +13,157 @@ class LMS{
     LMS *next;
 
     public:
-    void accept();
+    void createatlast();
     void display();
+    void search();
+    void updatenode();
+    void deletefirst();
+    void deletelast();
+    void deleteany();
+
 }*start=NULL,S;
 
-void LMS::accept(){
-    LMS *newnode,*temp;
-    newnode = new LMS;
-    newnode->next = NULL;
-    cout << "Enter the book details (Title , Auther , Publisher , Price ): ";
-    cin>>newnode->tt>>newnode->aut>>newnode->pub>>newnode->price;
+void LMS::createatlast(){
+    int ch1;
+    do{
+        LMS *newnode,*temp;
+        newnode = new LMS;
+        newnode->next = NULL;
+        cout << "Enter the book details (Title , Auther , Publisher , Price ): ";
+        cin>>newnode->tt>>newnode->aut>>newnode->pub>>newnode->price;
 
-    if (start == NULL){
-        start = newnode;
-        temp = start;
-    }
-    else{
-        while(temp != NULL){
-            temp = temp->next;
+        if (start == NULL){
+            start = newnode;
+            temp = start;
+            cout <<"Record stored successfully!\n";
         }
-        temp = newnode;
-    }
+        else{
+            while(temp != NULL){
+                temp = temp->next;
+            }
+            temp = newnode;
+            cout <<"Record stored successfully!\n";
+        }
+        cout<<"Do you want to accept more records? (1/0)  ";
+        cin>>ch1;
+    }while(ch1!=0);
 }
 
 void LMS::display(){
-    LMS *temp;
-    temp = start->next;
+    LMS *temp = start;
+    int cnt = 0;
     if (start == NULL ){
-        cout <<"No any record found!";
+        cout <<"No any record found!\n";
     }
     else{
-        while (temp != NULL){
-            cout<<"\n" << temp->tt << "\t" << temp->aut << "\t" << temp->pub << "\t" <<temp->price <<endl;
+        cout <<"\nTitle\tAuther\tPublisher\tPrice\n";
+        while(temp != NULL){
+            cout << temp->tt << "\t" << temp->aut << "\t" << temp->pub << "\t" <<temp->price <<endl;
+            cnt++;
             temp = temp->next;
         }
     }
-    
 }
+
+void LMS::search(){
+    string title;
+    LMS *temp = start;
+    int f = 0 , cnt = 0 ;
+    cout <<"Enter a book title to search: ";
+    cin>>title;
+
+    if (start == NULL ){
+        cout <<"No any record found!\n";
+    }
+    else{
+        while(temp != NULL){
+            if (temp->tt == tt){
+                f = 1;
+                cnt++;
+                cout <<"Book Found at position "<<cnt;
+                cout << temp->tt << "\t" << temp->aut << "\t" << temp->pub << "\t" <<temp->price <<endl;
+                break;
+            }
+            temp = temp->next;
+        }
+    }
+    if (f == 0){
+        cout <<"Book not found!";
+    }
+}
+
+void LMS::updatenode(){
+    string title;
+    LMS *temp = start;
+    int f = 0 , cnt = 0 ;
+    cout <<"\nEnter a book title to update: ";
+    cin>>title;
+
+    if (start == NULL ){
+        cout <<"\nNo any record found!\n";
+    }
+    else{
+        while(temp != NULL){
+            if (temp->tt == tt){
+                f = 1;
+                cnt++;
+                cout <<"\nBook Found at position " << cnt;
+                cout <<"\nEnter new information for this record: ";
+                cout <<"\nEnter the book details (Title , Auther , Publisher , Price ): ";
+                cin >> temp->tt >> temp->aut >> temp->pub >> temp->price ;
+                break;
+            }
+            temp = temp->next;
+        }
+    }
+    if (f == 0){
+        cout <<"\nBook not found to update !\n";
+    }
+}
+void LMS::deletefirst(){
+    LMS *temp = start;
+    if (start == NULL ){
+        cout <<"\nNo any record to delete !\n";
+    }
+    else{
+        temp = start;
+        start = temp->next;
+        cout << "\nRecord deleted successfully! \n";
+        delete temp;        
+    }
+}
+void LMS::deletelast(){
+
+}
+void LMS::deleteany(){
+
+}
+
 
 int main(){
     int ch,ch1;
     while(true){
-        cout <<"\n----MENU----\n1. Accept a book record\n2. Disaplay book record\n3. Exit\n";
+        cout <<"\n----MENU----\n1. Insert a book record\n2. Disaplay book record\n3. Search a book by its title\n4. Update the record\n5. Delete first node\n6. Exit\n";
         cout <<"Enter your choice: ";
         cin >>ch;
         switch(ch){
             case 1: 
-                do{
-                    S.accept();
-                    cout <<"\nRecord stored successfully!\n";
-                    cout<<"Do you want to accept more records? (1/0)  ";
-                    cin>>ch1;
-                }while(ch1!=0);
+                S.createatlast();
                 break;
             case 2:
                 S.display();
                 break;
             case 3:
-                cout <<"Exiting...";
+                S.search();
+                break;
+            case 4:
+                S.updatenode();
+                break;
+            case 5:
+                S.deletefirst();
+                break;
+            case 6:
+                cout <<"Exiting....";
                 break;
             default:
                 cout <<"\nEnter valid choice (1/2/3)\n";
